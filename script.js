369826f6b05f91669c930d1f3862ed1e82e04f95
Maksym Kintor <kintormaksim@gmail.com>
2023-02-09 13:15:22 +0100

    form

---
+++ script.js
@@ -0,0 +1,18 @@
+// Vrátí všechny Auta v autoserisu
+// Přidá auto do autoservisu
+// Smaže auto z autoservisu
+
+const auta = [];
+
+class Auto {
+    constructor(znacka, model, rok, kilometry) {
+        this.znacka = znacka;
+        this.model = model;
+        this.rok = rok;
+        this.kilometry = kilometry;
+    }
+}
+
+function AddCar(znacka, model, rok, kilometry){
+
+}
